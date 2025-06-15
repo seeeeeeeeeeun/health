e70e42a49db23b112e9d2a76bf02b226a6ee1a53
seeeeeeeeeeun <ytioil@hanyang.ac.kr>
2025-06-16 04:25:57 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.sidebar:
     st.markdown("<h3 style='color:#0077b6;'>💬 챗봇 질문 </h3>", unsafe_allow_html=True)
     st.markdown("""
     궁금한 점을 입력해보세요 :  
-    _(예: 실손보험으로 얼마 환급돼요? / 이 병원비는 공제 대상인가요?)_
+    _(예: 실손보험으로 얼마 환급되나요? / 이 병원비는 공제 대상인가요?)_
     """, unsafe_allow_html=True)
 
     question = st.text_input("")
